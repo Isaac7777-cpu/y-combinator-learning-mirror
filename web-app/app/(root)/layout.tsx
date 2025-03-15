832ddef776addaf7348ddfa27620a2d202209163
Isaac Leong <isaacleong23367@gmail.com>
2025-03-15 12:13:12 +1100

    Migrated Online Script to Work with Tailwindcss v4

--- web-app/app/(root)/layout.tsx
+++ web-app/app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <main className="font-work-sans">
+        <main className="font-work-sans bg-gray-50">
             <Navbar />
             {children}
         </main>
