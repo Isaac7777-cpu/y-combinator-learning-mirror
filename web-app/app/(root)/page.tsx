bdd2158af25b64e6b590df03a0c99a60353f7d93
Isaac Leong <isaacleong23367@gmail.com>
2025-03-16 08:20:57 +1100

    Completed Startup Cards and Installed Sanity

--- web-app/app/(root)/page.tsx
+++ web-app/app/(root)/page.tsx
@@ -1,10 +1,24 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
     searchParams: Promise<{ query?: string }>
 }) {
-    
+
     const query = (await searchParams).query;
+
+    const posts = [
+        {
+            _createdAt: new Date(),
+            views: 95,
+            author: { _id: 1, name: "Isaac Newton" },
+            _id: 1,
+            description: "This is a description",
+            image: "https://upload.wikimedia.org/wikipedia/en/c/ca/Steins%3BGate_anime_cover.png",
+            category: "Anime",
+            title: "Anime2 Studio"
+        }
+    ];
 
     // We can directly do the search here. 
     // However, given that it likely to require some client side information, it is better to separate that 
@@ -19,7 +33,23 @@ export default async function Home({ searchParams }: {
                     Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
                 </p>
 
-                <SearchForm query={query}/>
+                <SearchForm query={query} />
+            </section>
+
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for "${query}"` : 'All Startups'}
+                </p>
+
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post: StartupCardType, number) => (
+                            <StartupCard key={post?._id} post={post} />
+                        ))
+                    ): (
+                        <p className="no-result">No startups found</p>
+                    )}
+                </ul>
             </section>
         </>
     );
--- web-app/eslint.config.mjs
+++ web-app/eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" }, // Ignore variables starting with _
+      ],
+      indent: ["error", 4],
+    },
+  },
 ];
 
 export default eslintConfig;
