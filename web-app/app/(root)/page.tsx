2990d1f1e9e355ebe0e54fa76e7d967aaf9f697f
Isaac Leong <isaacleong23367@gmail.com>
2025-03-15 23:34:19 +1100

    Updated for Shadcn Update
    I think basically everytime he touches the tailwind related configs, you
    have to do the drop down trick and re-bump the version with the
    migration command to make sure everything is working. It is clearly not
    very ideal thou...
    
    Hope someone needs the fix can see my command

--- web-app/app/(root)/page.tsx
+++ web-app/app/(root)/page.tsx
@@ -1,9 +1,14 @@
 export default function Home() {
     return (
         <>
-            <h1 className="uppercase bg-black px-6 py-3 font-worksans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
-                HOME
-            </h1>
+            <section className="pink_container">
+                <h1 className="heading">
+                    Pitch Your Startup, <br /> Connect With Entrepreneurs
+                </h1>
+                <p className="sub-heading max-w-3xl!">
+                    Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+                </p>
+            </section>
         </>
     );
 }
